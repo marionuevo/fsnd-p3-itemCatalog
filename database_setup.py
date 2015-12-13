51c4e06a2759db6e9e6bded3f534aa1af79758a2
Mario Nuevo <marionuevo@gmail.com>
2015-12-13 19:36:21 +0000

    Initial GIT commit.

---
+++ database_setup.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Style(Base):
+    __tablename__ = 'style'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
+
+class Model(Base):
+    __tablename__ = 'model'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    power = Column(String(250))
+    style_id = Column(Integer, ForeignKey('style.id'))
+    style = relationship(Style)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'power': self.power
+        }
+
+engine = create_engine('sqlite:///motorbikes.db')
+
+Base.metadata.create_all(engine)
