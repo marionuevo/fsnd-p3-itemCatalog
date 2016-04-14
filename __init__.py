8602a511a54fa6d7234c4d90569183e4986e4a58
marionuevo <marionuevo@gmail.com>
2016-04-14 12:52:06 +0100

    None

--- __init__.py
+++ __init__.py
@@ -103,7 +103,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(APP_PATH + '/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
